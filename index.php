86db6926bc82b8e69440040a8fc4c199490ca081
Kenneth <kyleabs20@gmail.com>
2021-01-08 20:49:00 -0800

    dot

--- index.php
+++ index.php
@@ -10,15 +10,15 @@ switch ($text) {
     case '':
         // TODO: this is the frist request// note how we start with CON
         $response = "CON what would you like to check /n"; 
-        $response = "1. My Account Number";
-        $response = "2. My Phone Number"; 
+        $response .= "1. My Account Number";
+        $response .= "2. My Phone Number"; 
         break;
 
     case '1':
         // TODO: this is the frist request// note how we start with CON
         $response = "CON Choose account information you want to view  /n"; 
-        $response = "1. My Account Number";
-        $response = "2. My Phone Number"; 
+        $response .= "1. My Account Number";
+        $response .= "2. My Account Balance"; 
         break;
 
     case '':
