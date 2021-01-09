c4e55530846725a32e52bc167f0138e2842cc4bd
Kenneth <kyleabs20@gmail.com>
2021-01-08 20:38:23 -0800

    playing around ussd

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php
 
-// declaring variables 
+// declaring variables sent via post
+$session_id = $_POST['sessionId'];
+$service_code = $_POST['service_code'];
+$phone_number = $_POST['phone'];
+$text = $_POST['text'];
+
+switch ($text) {
+    case '':
+        // TODO: this is the frist request// note how we start with CON
+        $response = "CON what would you like to check /n"; 
+        $response = "1. My Account Number";
+        $response = "2. My Phone Number"; 
+        break;
+
+    case '1':
+        // TODO: this is the frist request// note how we start with CON
+        $response = "CON Choose account information you want to view  /n"; 
+        $response = "1. My Account Number";
+        $response = "2. My Phone Number"; 
+        break;
+
+    case '':
+        // TODO: this is the frist request// note how we start with CON
+        $response = "CON what would you like to check /n"; 
+        $response = "1. My Account Number";
+        $response = "2. My Phone Number"; 
+        break;   
+    
+    default:
+        # code...
+        break;
+}
 
 ?>
